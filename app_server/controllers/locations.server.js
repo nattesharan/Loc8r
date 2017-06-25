5ee8062c6db996212c33427c6fe2f0e487b728cb
sharanNatte <sharan@vitwit.com>
2017-06-25 15:19:15 +0530

    Added template for adding review

--- app_server/controllers/locations.server.js
+++ app_server/controllers/locations.server.js
@@ -2,8 +2,8 @@ exports.homeList = function(req, res) {
     res.render('locations-list');
 };
 exports.locationInfo = function(req, res) {
-    res.render('location-info', { title: 'For location Info' });
+    res.render('location-info');
 };
 exports.addReview = function(req, res) {
-    res.render('index', { title: 'For adding review' });
+    res.render('location-review');
 };
