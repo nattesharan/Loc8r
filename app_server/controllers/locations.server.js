88496b6930b8c70281b1415b6be22345a1340412
sharanNatte <sharan@vitwit.com>
2017-06-25 14:56:28 +0530

    Added templates

--- app_server/controllers/locations.server.js
+++ app_server/controllers/locations.server.js
@@ -1,8 +1,8 @@
 exports.homeList = function(req, res) {
-    res.render('index', { title: 'For Home List' });
+    res.render('locations-list');
 };
 exports.locationInfo = function(req, res) {
-    res.render('index', { title: 'For location Info' });
+    res.render('location-info', { title: 'For location Info' });
 };
 exports.addReview = function(req, res) {
     res.render('index', { title: 'For adding review' });
